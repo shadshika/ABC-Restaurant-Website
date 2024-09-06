58c63ad3e465be815788228691d03fa363c4050c
shadshika <142075386+shadshika@users.noreply.github.com>
2024-09-06 19:45:11 +0530

    Add files via upload

---
+++ Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "abc_restaurant";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+    public function connect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
